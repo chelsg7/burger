be9488d8238f22daaf0080d07a773a08c61bf239
thorc <chelsgieck@gmail.com>
2018-06-21 15:46:10 -0400

    burger

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese beef burger', true);
+INSERT INTO burgers (burger_name) VALUES ('fish-O-filet burger');
+INSERT INTO burgers (burger_name) VALUES ('chicken burger');
